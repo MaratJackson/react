c752f3b22070ad05dd243b29c9cec9e63f06dfd8
MaratJackson <35914445+MaratJackson@users.noreply.github.com>
2019-10-09 14:06:59 +0300

    Update CommentsList.jsx

--- src/components/CommentsMenu/CommentsList/CommentsList.jsx
+++ src/components/CommentsMenu/CommentsList/CommentsList.jsx
@@ -5,20 +5,20 @@ import style from './CommentsList.module.css';
 let CommentsList = (props) => {
 
     let getComments = props.data.map(function(elem){
-          return <ul>
-               <li>1 2 3 4 6</li>
-               <li className={style.text}>{elem.text} </li>
-               </ul>
+          return <div>
+                   <p><b className={style.nameUser}>{elem.name}</b></p>
+                   <br></br>
+                   <ul>
+                     <li></li>
+                     <li className={style.text}>{elem.text} </li>
+                   </ul>
+                 </div>
         })
     
 
     return (
         <div>
-           <p> 
-             <b className={style.nameUser}>Nicname</b>
-           </p>
-           <br></br>
-             {getComments}
+         {getComments}
         </div>
     )
 }
