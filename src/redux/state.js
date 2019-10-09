bb0ff79347e5181aa25d85ad1095dd12213ce9bd
MaratJackson <35914445+MaratJackson@users.noreply.github.com>
2019-10-09 14:05:59 +0300

    Update state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,5 @@
-const ADD_COMMENT = 'ADD_COMMENT'
-const UPDATE_NEW_TEXT_COMMENT = 'UPDATE_NEW_TEXT_COMMENT'
-const UPDATE_NEW_NAME_COMMENT = 'UPDATE_NEW_NAME_COMMENT'
+import createCommentReduce from './createCommentReduce'
+// import CreateComment from '../components/CreateComment/CreateComment'
 
 let store = {
 
@@ -24,58 +23,15 @@ let store = {
         }
       },
     
-
-  addComment(){
-      let commentsArr = this._state.comments
-      let newCommentData = this._state.newCommentData
-      let newId = commentsArr.length + 1
-      let comment = {
-         id:newId,
-         name:newCommentData.name,
-         text:newCommentData.text,
-         date:new Date()
-      }
-      commentsArr.push(comment)
-      this.renderApp(this._state)
-      console.log(commentsArr)      
-  },
-
- 
-  updateText(text) {
-      this._state.newCommentData.text = text
-      this.renderApp(this._state)
-  },
-  
-  updateName(name) {
-      this._state.newCommentData.name = name
-      this.renderApp(this._state) 
-  },
-
-
   dispatch(action){
-      if (action.type === ADD_COMMENT) {
-         this.addComment()
-      } else if (action.type === UPDATE_NEW_TEXT_COMMENT) {
-         this.updateText(action.text)
-      } else if (action.type === UPDATE_NEW_NAME_COMMENT) {
-         this.updateName(action.name)
-      }
+      this._state = createCommentReduce(this._state, action)
+      this.renderApp(this._state)
   },  
 
   subscribe(observer){
       this.renderApp = observer
     }
 
- }
-
- export const addNewComment = () => ({type:ADD_COMMENT})
-
- export const updateNewCommentText = (text) => {
-     return {type:UPDATE_NEW_TEXT_COMMENT, text:text}
- }
-
- export const updateNewCommentName = (name) => {
-     return {type:UPDATE_NEW_NAME_COMMENT, name:name}
  }
 
 export default store
